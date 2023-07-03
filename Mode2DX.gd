23850f439f56f3b74aac154b3220a38fe40fe401
Brom Bresenham <brom.bresenham@runegate.com>
2023-07-03 15:35:49 -0400

    [v1.0]

---
+++ Mode2DX.gd
@@ -0,0 +1,71 @@
+## Mode2DX allows standard pixel coordinates to be used in a 3D scene. Add a Mode2DX node to the
+## scene and assign the scene's Camera3D to it. The positions of children you add to Mode2DX are
+## treated as pixel coordinates, with (0,0) being the top-left of the display. Two caveats: 1) Each
+## child node or subtree root must have a negative Y scale. 2) Always take the abs() of a child
+## nodes's scale before using it in a computation (applies to X, Y, and Z scale components). Mode2DX
+## makes itself a unique name so that other scripts can access it with '$%Mode2DX' AKA
+## 'get_node("%Mode2DX")' (or whatever this node is renamed to).
+extends Node3D
+
+class_name Mode2DX
+
+## Mode2DX receives the viewport's 'size_changed' signal and then updates the 2DX display settings
+## before emitting the 'display_size_changed' signal. Connecting to this signal ensures that
+## Mode2DX.display_size is up to date before the receiver performs any layout adjustments.
+signal display_size_changed(Vector2)
+
+## The Camera3D that will be automatically adjusted to use pixel coordinates.
+@export var camera : Camera3D
+
+## The current size of the viewport, in pixels. (0,0) is the top-left corner.
+var display_size : Vector2
+
+## The depth at which 3D model size matches pixel size 1:1. The Mode2DX node is positioned at this
+## depth, so a child node with z=0 will have a 1:1 pixel size, a node with z=nominal_z*0.9 will be
+## closer to the camera, and a node with z=nominal_z*1.1 will be farther from the camera.
+##
+## Smaller nominal_z values exaggerate the effects of rotating into or moving along the Z axis,
+## while larger nominal_z values reduce the depth of the 3D effect.
+@export var nominal_z = 2000:
+	set(value):
+		if value != nominal_z: nominal_z=value; update_display_settings()
+
+## The far clipping plane. Note that the near clipping plane is fixed at 1.
+@export var z_far = 4000:
+	set(value):
+		if value != z_far: z_far=value; update_display_settings()
+
+func update_display_settings():
+	# Called automatically on _ready(), when the viewport size changes, and from _process(). Can be
+	# called manually e.g. to update 'display_size' immediately after the current camera is changed.
+	if not camera:
+		printerr( "[Mode2DX] No camera assigned." )
+		return
+
+	var viewport = get_viewport()
+	if not viewport: return
+
+	display_size = viewport.get_visible_rect().size
+
+	var k = (nominal_z + 1)
+	var size = display_size.y / k
+	camera.set_frustum( size, Vector2(), 1, z_far )
+
+	var offset = display_size/2
+	position = Vector3( -offset.x, offset.y, -nominal_z )
+	rotation = Vector3()
+	scale = Vector3( 1, -1, 1 )
+
+	print( str("[Mode2DX] ", camera.name, ".set_frustum(",size,",Vector2(),1,",z_far,")") )
+	print( str("[Mode2DX] ", name, ".position = ",position) )
+	print( str("[Mode2DX] ", name, ".scale = ",scale) )
+
+	display_size_changed.emit( display_size )
+
+func _ready():
+	unique_name_in_owner = true  # enable access using $%Mode2DX and get_node("%Mode2DX")
+	get_viewport().connect( "size_changed", _on_viewport_size_changed )
+	update_display_settings()
+
+func _on_viewport_size_changed():
+	update_display_settings()
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Mode2DX
-A Godot node that allows a 3D scene to use a standard 2D coordinate system
+A Godot node that allows a 3D scene to use a standard 2D coordinate system.
+
+About     | Current Release
+----------|-----------------------
+Version   | 1.0
+Date      | July 5, 2023
+Platform  | Godot 4
+License   | [MIT License](LICENSE)
+Author    | Brom Bresenham
+
+# Usage
+
+- Create a 3D scene.
+- Add a Camera3D (default configuration is fine).
+- Add [Mode2DX.gd](Mode2DX.gd) as a scene node.
+- Drag the camera into the `camera` property of the `Mode2DX` node.
+- All children and subtrees of `Mode2DX` will use 2D coordinates.
+- (0,0) is top-left of the display.
+- `$%Mode2DX.display_size` contains the current window size in pixels.
+- `Mode2DX` will automatically update its configuration if the screen size changes.
+- The scale * mesh size of each descendent node will be the pixel size it is drawn at.
+    - For example, a `MeshInstance3D` quad scaled to 100x50x1 will be 100x50 pixels onscreen (if positioned at `z=0` as a child of `Mode2DX`).
+- Because `Mode2DX` inverts the Y axis relative to standard 3D scenes, caveats apply:
+    - The `scale.y` property of each child node or subtree root must be negatated. For example, to have a quad be drawn at 100x50 pixels you would set its `scale` to `(100,-50,1)`.
+    - You should always take the `abs()` of a `Mode2DX` child node's `scale` before using it in computation. Because of the way that Godot stores 3D angles and scale in a combined "basis" form, you might set `rotation=(0,0,0), scale=(1,-1,1)` in the editor but when the game runs the properties would be `rotation:(0,180,0), scale:(-1,-1,-1)`.
+- Mode2DX pairs well with the [3D Drawing Order Godot Engine modification](https://github.com/brombres/Godot-3D-Drawing-Order).
+
+# Sample Project
+This repo contains a sample project made in Godot 4.1-RC1. The only necessary file for your own projects is [Mode2DX.gd](Mode2DX.gd), which you can copy out separately.
