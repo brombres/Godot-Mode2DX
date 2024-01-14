dcb17272690869056cff6000d81ceb8e180b34e9
Brom Bresenham <brom.bresenham@runegate.com>
2024-01-14 14:45:40 -0500

    [Plugin] Pluginified

---
+++ addons/Mode2DX/plugin.gd
@@ -0,0 +1,8 @@
+@tool
+extends EditorPlugin
+
+func _enter_tree():
+	add_custom_type( "Mode2DX", "Node3D", preload("Mode2DX.gd"), preload("Mode2DX.svg") )
+
+func _exit_tree():
+	remove_custom_type( "Mode2DX" )
