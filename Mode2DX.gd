61b9a4f48cacd06949d0bd443451a4ac59eb8ebf
Brom Bresenham <brom.bresenham@runegate.com>
2024-01-14 14:13:35 -0500

    [Mode2DX] Updated

--- Mode2DX.gd
+++ Mode2DX.gd
@@ -1,16 +1,8 @@
-# Mode2DX.gd
-# v1.1
-# July 7, 2023 by Brom Bresenham
+@tool
 
-## Mode2DX allows standard pixel coordinates to be used in a 3D scene. Add a Mode2DX node to the
-## scene and assign the scene's Camera3D to it. The positions of children you add to Mode2DX are
-## treated as pixel coordinates, with (0,0) being the top-left of the display. Two caveats: 1) Each
-## child node or subtree root must have a negative Y scale. 2) Always take the abs() of a child
-## nodes's scale before using it in a computation (applies to X, Y, and Z scale components). Mode2DX
-## makes itself a unique name so that other scripts can access it with '$%Mode2DX' AKA
-## 'get_node("%Mode2DX")' (or whatever this node is renamed to).
-class_name Mode2DX
-extends Node3D
+## Mode2DX allows standard pixel coordinates to be used in a 3D scene. Child nodes of a
+## Mode2DX node will use coordinates that match viewport pixel coordinates 1:1.
+class_name Mode2DX extends Node3D
 
 ## Mode2DX receives the viewport's 'size_changed' signal and then updates the 2DX display settings
 ## before emitting the 'display_size_changed' signal. Connecting to this signal ensures that
@@ -18,54 +10,125 @@ extends Node3D
 signal display_size_changed(Vector2)
 
 ## The Camera3D that will be automatically adjusted to use pixel coordinates.
-@export var camera : Camera3D
+@export var camera : Camera3D :
+	set(value):
+		if camera != value:
+			camera = value
+			if Engine.is_editor_hint() and camera and _is_ready:
+				print( "[Mode2DX] Camera connected. Reselect the camera and add a checkmark to the 'Preview' box in the Editor viewport to see the scene in 2DX." )
 
 ## The current size of the viewport, in pixels. (0,0) is the top-left corner.
 var display_size : Vector2
 
+var _is_ready := false
+
 ## The depth at which 3D model size matches pixel size 1:1. The Mode2DX node is positioned at this
-## depth, so a child node with z=0 will have a 1:1 pixel size, a node with z=nominal_z*0.9 will be
-## closer to the camera, and a node with z=nominal_z*1.1 will be farther from the camera.
+## depth, so a child node with z=0 will have a 1:1 pixel size, a node with a positive [code]z[/code] will be
+## closer to the camera, and a node with negative [code]z[/code] will be farther from the camera.
 ##
-## Smaller nominal_z values exaggerate the effects of rotating into or moving along the Z axis,
-## while larger nominal_z values reduce the depth of the 3D effect.
+## Smaller [member nominal_z] values exaggerate the effects of rotating into or moving along the Z axis,
+## while larger [member nominal_z] values reduce the depth of the 3D effect.
 @export var nominal_z = 2000:
 	set(value):
-		if value != nominal_z: nominal_z=value; update_display_settings()
+		if nominal_z != value:
+			nominal_z = value
+			if _is_ready: update_display_settings( true )
 
 ## The far clipping plane. Note that the near clipping plane is fixed at 1.
 @export var z_far = 4000:
 	set(value):
-		if value != z_far: z_far=value; update_display_settings()
+		if z_far != value:
+			z_far = value
+			if _is_ready: update_display_settings( true )
 
-func update_display_settings():
-	# Called automatically on _ready(), when the viewport size changes, and from _process(). Can be
-	# called manually e.g. to update 'display_size' immediately after the current camera is changed.
+## Enabling this setting will put the (0,0) origin in the top-left corner instead of the bottom-left
+## corner for child nodes of this Mode2DX node, and down will now be y-positive instead of y-negative.
+## This matches the orientation conventional 2D coordinate systems. However, child nodes will appear
+## inverted by default, so they must be flipped or rotated 180 degrees on the Y axis. It may be
+## easier to leave [member top_left_origin] off and utlize [method place] and [method get_placement]
+## to set and get node positions in top-left-relative coordinates.
+@export var top_left_origin := false :
+	set(value):
+		if top_left_origin != value:
+			top_left_origin = value
+			if _is_ready: update_display_settings( true )
+
+## Returns the specified node's position as [Vector2] relative to a top-left origin of [code](0,0)[/code],
+## regardless of whether [member top_left_origin] is set or not. [param container_size] is optional and may be
+## used to specify the width and height of the local coordinate system; [member display_size] is used if
+## [param container_size] is unspecified.
+func get_placement( node:Node3D, container_size:Variant=null )->Vector2:
+	var result = Vector2( node.position.x, node.position.y )
+	if top_left_origin: return result
+	else :              return invert_y( result, container_size )
+
+## Inverts the Y coordinate of a vector, [Rect2], or [AABB], relative to the viewport display size
+## or the specified [param container_size]. [method get_placement] and [method place] utilize this method.
+## Call it directly to invert a [Rect2] or [AABB].
+func invert_y( pos:Variant, container_size:Variant=null )->Variant:
+	if not container_size: container_size = display_size
+	match typeof(pos):
+		TYPE_VECTOR2:   pos.y = container_size.y - pos.y
+		TYPE_VECTOR2I:  pos.y = container_size.y - pos.y
+		TYPE_RECT2:     pos.position.y = container_size.y - pos.end.y
+		TYPE_RECT2I:    pos.position.y = container_size.y - pos.end.y
+		TYPE_VECTOR3:   pos.y = container_size.y - pos.y
+		TYPE_VECTOR3I:  pos.y = container_size.y - pos.y
+		TYPE_VECTOR4:   pos.y = container_size.y - pos.y
+		TYPE_VECTOR4I:  pos.y = container_size.y - pos.y
+		TYPE_AABB:      pos.position.y = container_size.y - pos.end.y
+	return pos
+
+## Places [param node] at the specified position. [param pos] is a [Vector2] or [Vector3] that
+## is relative to a top-left [code](0,0)[/code] origin, regardless of whether [member top_left_origin]
+## is set or not. [param container_size] is optional and may be used to specify the width and height
+## of the local coordinate system; [member display_size] is used if [param container_size] is unspecified.
+func place( node:Node3D, pos:Variant, container_size:Variant=null ):
+	pos = Vector3( pos.x, pos.y, node.position.z )
+	if top_left_origin: node.position = pos
+	else:               node.position = invert_y( pos, container_size )
+
+## Called automatically on [method _ready] and when the viewport size changes. Can be
+## called manually e.g. to update [member display_size] immediately after the current camera is changed.
+## Pass [code]true[/code] to force an update even if the viewport size hasn't changed.
+func update_display_settings( force_update:bool=false ):
 	if not camera:
-		printerr( "[Mode2DX] No camera assigned." )
+		if not Engine.is_editor_hint(): printerr( "[Mode2DX] No camera assigned." )
 		return
 
-	var viewport = get_viewport()
+	var viewport = EditorInterface.get_editor_viewport_3d(0) if Engine.is_editor_hint() else get_viewport()
 	if not viewport: return
 
-	display_size = viewport.get_visible_rect().size
+	var new_display_size = viewport.get_visible_rect().size
+	if display_size == new_display_size and not force_update: return
+	display_size = new_display_size
 
 	var k = (nominal_z + 1)
 	var size = display_size.y / k
 	camera.set_frustum( size, Vector2(), 1, z_far )
 
 	var offset = display_size/2
-	position = Vector3( -offset.x, offset.y, -nominal_z )
+	if top_left_origin: offset.y = -offset.y
+	position = Vector3( -offset.x, -offset.y, -nominal_z )
 	rotation = Vector3()
-	scale = Vector3( 1, -1, 1 )
+	scale = Vector3( 1, (-1 if top_left_origin else 1), 1 )
 
-	print( str("[Mode2DX] ", camera.name, ".set_frustum(",size,",Vector2(),1,",z_far,")") )
+	print( str("[Mode2DX] ", camera.name, ".set_frustum(",size,",Vector2(0,0),1,",z_far,")") )
 	print( str("[Mode2DX] ", name, ".position = ",position) )
 	print( str("[Mode2DX] ", name, ".scale = ",scale) )
 
-	display_size_changed.emit( display_size )
+	if Engine.is_editor_hint():
+		for connection in display_size_changed.get_connections():
+			if connection.callable.get_object().get_script().is_tool():
+				connection.callable.call( display_size )
+	else:
+		display_size_changed.emit( display_size )
+
+func _process( _dt ):
+	if Engine.is_editor_hint(): update_display_settings()
 
 func _ready():
+	_is_ready = true
 	unique_name_in_owner = true  # enable access using $%Mode2DX and get_node("%Mode2DX")
 	get_viewport().connect( "size_changed", _on_viewport_size_changed )
 	update_display_settings()
--- README.md
+++ README.md
@@ -3,21 +3,23 @@ A Godot node that allows a 3D scene to use a standard 2D coordinate system.
 
 About     | Current Release
 ----------|-----------------------
-Version   | 1.1
-Date      | July 7, 2023
-Platform  | Godot 4
+Version   | 2.0
+Date      | January 14, 2024
+Platform  | Godot 4.x
 License   | [MIT License](LICENSE)
 Author    | Brom Bresenham
 
 # Usage
 
 - Create a 3D scene.
-- Add a Camera3D (default configuration is fine).
-- Add [Mode2DX.gd](Mode2DX.gd) as a scene node.
+- Add a `Camera3D` (default configuration is fine).
+- Add a `Mode2DX` node to the scene.
 - Drag the camera into the `camera` property of the `Mode2DX` node.
-- All children and subtrees of `Mode2DX` will use 2D coordinates.
-- (0,0) is top-left of the display.
-- `$%Mode2DX.display_size` contains the current window size in pixels.
+- Add `Sprite3D` and other visual 3D nodes as children or descendants of the `Mode2DX` node.
+- `(0,0)` is at the bottom-left of the viewport by default, with positive Y oriented up.
+- Enabling the `top_left_origin` property will place `(0,0)` at the top-left with positive Y oriented down. The caveat is that children of `Mode2DX` will appear inverted, so they must be flipped or rotated 180º on the Y axis.
+- `$%Mode2DX.display_size` contains the current viewport size in pixels.
+- Method `place(node:Node3D,position:Vector2,[container_size:Vector2])` is a utility method that places a `Node3D` at the specified pixel position using a top-left-origin coordinate system regardless of whether `top_left_origin` is enabled or not. The `display_size`
 - `Mode2DX` will automatically update its configuration if the screen size changes.
 - The scale * mesh size of each descendent node will be the pixel size it is drawn at.
     - For example, a `MeshInstance3D` quad scaled to 100x50x1 will be 100x50 pixels onscreen (if positioned at `z=0` as a child of `Mode2DX`).
@@ -26,5 +28,30 @@ Author    | Brom Bresenham
     - You should always take the `abs()` of a `Mode2DX` child node's `scale` before using it in computation. Because of the way that Godot stores 3D angles and scale in a combined "basis" form, you might set `rotation=(0,0,0), scale=(1,-1,1)` in the editor but when the game runs the properties would be `rotation:(0,180,0), scale:(-1,-1,-1)`.
 - Mode2DX pairs well with the [3D Drawing Order Godot Engine modification](https://github.com/brombres/Godot-3D-Drawing-Order).
 
-# Sample Project
-This repo contains a sample project made in Godot 4.1-RC1. The only necessary file for your own projects is [Mode2DX.gd](Mode2DX.gd), which you can copy out separately.
+# API
+
+## $%Mode2DX
+
+- `Mode2DX` configures itself to make its name unique to the scene.
+- In any script you can reference `$%Mode2DX` to retrieve the scene's `Mode2DX` node (equivalent to `get_node("%Mode2DX")`.
+- Consider saving a local reference to `Mode2DX` as an `@onready` variable to reduce the overhead of repeated calls to `get_node()`.
+- For example: `@onready var mode_2dx = $%Mode2DX`.
+
+## Properties
+
+Name              | Type       | Default | Description
+------------------|------------|---------|-----------------------------
+`camera`          | `Camera3D` |         | The `Camera3D` that will be automatically adjusted to use pixel coordinates.
+`display_size`    | `Vector2`  |         | The current size of the viewport, in pixels. `(0,0)` is the top-left corner and positive Y is down if `top_left_origin` is `true`, otherwise `(0,0)` is the bottom-left corner and positive Y is up.
+`nominal_z`       | `float`    | `2000`  | The depth at which 3D model size matches pixel size 1:1. The Mode2DX node is positioned at this depth, so a child node with z=0 will have a 1:1 pixel size, a node with a positive `z` will be closer to the camera, and a node with negative `z` will be farther from the camera.<br><br>Smaller `nominal_z` values exaggerate the effects of rotating into or moving along the Z axis, while larger `nominal_z` values reduce the depth of the 3D effect.
+`top_left_origin` | `bool`     | `false` | Enabling this setting will put the `(0,0)` origin in the top-left corner instead of the bottom-left corner for child nodes of this `Mode2DX` node, and down will now be y-positive instead of y-negative. This matches the orientation conventional 2D coordinate systems. However, child nodes will appear inverted by default, so they must be flipped or rotated 180º on the Y axis.  It may be easier to leave `top_left_origin` off and utlize `place()` and `get_placement()` to set and get node positions using top-left-relative coordinates.
+`z_far`           | `float`    | `4000`  | The far clipping plane. Note that the near clipping plane is fixed at `1`.
+
+## Methods
+
+Signature         | Description
+------------------|-------------
+`get_placement(node:Node3D,[container_size:Vector2])->Vector2` | Returns the specified node's position as [Vector2] relative to a top-left origin of `(0,0)`, regardless of whether `top_left_origin` is set or not. `container_size` is optional and may be used to specify the width and height of the local coordinate system; `display_size` is used if `container_size` is unspecified.
+`invert_y(pos:Variant,[container_size:Vector2])->Variant` | Inverts the Y coordinate of a vector, `Rect2`, or `AABB`, relative to the viewport display size or the specified `container_size`. `get_placement()` and `place()` utilize this method. Call it directly invert a `Rect2` or `AABB`.
+`place(node:Node3D,position:Variant,[container_size:Vector2])` | Places `node` at the specified position. `pos` is a `Vector2` or `Vector3` that is relative to a top-left `code`(0,0)`/code` origin, regardless of whether `top_left_origin` is set or not. `container_size` is optional and may be used to specify the width and height of the local coordinate system; `display_size` is used if `container_size` is unspecified.
+`update_display_settings([force_update:bool])` | Called automatically on `_ready()` and when the viewport size changes. Can be called manually e.g. to update `display_size` immediately after the current camera is changed.  Pass `true` to force an update even if the viewport size hasn't changed.
